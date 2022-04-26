f374fd36d0917035bebddfe18d940304ef337504
kisszaya <sveta.matvik.sveta@gmail.com>
2022-04-26 06:24:47 +0300

    feat(src): base config and dir structure

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/index'
+
+function App() {
+    return (
+        <div className="App">
+            Meow
+        </div>
+    );
+}
+
+export default App;
